7c088dd6d792512561f8dc6f59a25c2ce2f36959
zesleepyhead <nontakorn.viji@bumail.net>
2024-03-25 22:07:36 +0700

    Changed Black Hole Script

--- Assets/Script/BlackHole.cs
+++ Assets/Script/BlackHole.cs
@@ -4,33 +4,79 @@ using UnityEngine;
 
 public class BlackHole : MonoBehaviour
 {
-    public float gravitationalConstant = 6.67f;  // Gravitational constant G
-    public float planetMass = 100f;               // Mass of the planet
-    public float radius = 10f;                          // Radius of planet influence
+    public Rigidbody rb;
+ 
+    private const float G = 6.67f;
+ 
+    public static List<BlackHole> pAttraction;
 
-    // Update is called once per frame
+    void AttractorFormular(BlackHole other )
+    {
+        Rigidbody rbOther = other.rb;
+ 
+        Vector3 direction = rb.position - rbOther.position;
+ 
+        float distance = direction.magnitude;
+ 
+        // F = G * (M1*m2)/D^2
+        float forceMagnitude = G *( rb.mass * rbOther.mass ) / Mathf.Pow( distance , 2 ) ;
+ 
+        Vector3 forceDir = direction.normalized * forceMagnitude;
+ 
+        rbOther.AddForce(forceDir);
+        
+ 
+    }//AttractorFormular
+
     void FixedUpdate()
     {
-        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
-
-        foreach (Collider collider in colliders)
+        foreach ( var attraction in pAttraction)
         {
-            Rigidbody rb = collider.GetComponent<Rigidbody>();
-
-            if (rb != null)
+            if (attraction != this)
             {
-                Vector3 direction = transform.position - collider.transform.position;
-                float distance = direction.magnitude;
-
-                if (distance > 0)  // Avoid division by zero
-                {
-                    // Calculate the gravitational force
-                    float forceMagnitude = gravitationalConstant * (planetMass * rb.mass) / (distance * distance);
-
-                    // Apply the gravitational force to the object
-                    rb.AddForce(direction.normalized * forceMagnitude, ForceMode.Force);
-                }
+                AttractorFormular(attraction);
             }
         }
-    }
+    }//FixedUpdate
+
+    private void OnEnable()
+    {
+        if ( pAttraction == null )
+        {
+            pAttraction = new List<BlackHole>();
+        }
+ 
+        pAttraction.Add(this);
+ 
+    }//
+
+    // public float gravitationalConstant = 6.67f;  // Gravitational constant G
+    // public float planetMass = 100f;               // Mass of the planet
+    // public float radius = 10f;                          // Radius of planet influence
+
+    // // Update is called once per frame
+    // void FixedUpdate()
+    // {
+    //     Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+
+    //     foreach (Collider collider in colliders)
+    //     {
+    //         Rigidbody rb = collider.GetComponent<Rigidbody>();
+
+    //         if (rb != null)
+    //         {
+    //             Vector3 direction = transform.position - collider.transform.position;
+    //             float distance = direction.magnitude;
+
+    //             if (distance > 0)  // Avoid division by zero
+    //             {
+    //                 // Calculate the gravitational force
+    //                 float forceMagnitude= gravitationalConstant * (planetMass * rb.mass) / (distance * distance);
+
+    //                 // Apply the gravitational force to the object
+    //                 rb.AddForce(direction.normalized * forceMagnitude, ForceMode.Force);
+    //             }
+    //         }
+    //     }
+    // }
 }
